e889299223c8086c72ccd15e23b7c3daa39d58af
teomio02 <tmiozzi2002@gmail.com>
2025-05-29 18:46:25 +0200

    added FSDao

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
+
