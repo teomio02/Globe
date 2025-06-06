838f28b43da1f20cd00c8267ebcca61c019bae3d
teomio02 <tmiozzi2002@gmail.com>
2025-06-06 17:07:50 +0200

    fix some code smell

--- src/main/java/it/uniroma2/ispw/globe/controller/guicontroller/PaymentGUIController.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma2.ispw.globe.controller.guicontroller;
-
-public class PaymentGUIController {
-    private String sessionId;
-
-    public PaymentGUIController(String sessionId) {
-        this.sessionId = sessionId;
-    }
-}
